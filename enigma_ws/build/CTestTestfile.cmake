7a888d9adc8d0847ce18a8b0bfe1b3ce18c7a78c
Gonzalo Diaz <egonzalodm@gmail.com>
2020-07-20 12:20:25 -0500

    Creacion del Workspace ROS

---
+++ enigma_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /media/gonzalo/Archivos/Universidad/Codigo/Enigma/enigma_ws/src
+# Build directory: /media/gonzalo/Archivos/Universidad/Codigo/Enigma/enigma_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
